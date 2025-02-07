0f28fb704b0eeba5cc23eca1b6e0114f9d6d5add
Nirakash <83993087+Niracash@users.noreply.github.com>
2025-02-07 14:33:06 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -163,12 +163,12 @@
 </div>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Comic+Sans+MS:wght@400&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400&display=swap');
 
     :global(body) {
         margin: 0;
         padding: 0;
-        font-family: 'Comic Sans MS', cursive;
+        font-family: 'Quicksand', sans-serif;
     }
 
     .shadow-text {
